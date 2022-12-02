ff10113e4e6ddb9942b3cc0b135b19e6a6548ac0
Юрий Богомолов <yb85arm@yandex.ru>
2022-12-02 17:02:17 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack (char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
     if char_class == 'mage':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}')
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
-def defence(char_name, char_class):
+
+def defence (char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-def special(char_name, char_class):
+def special (char_name, char_class):
     if char_class == 'warrior': 
         return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
@@ -26,7 +27,7 @@ def special(char_name, char_class):
 
 
 
-def start_training(char_name, char_class):
+def start_training (char_name, char_class):
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
